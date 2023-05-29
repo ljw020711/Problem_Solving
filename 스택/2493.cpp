8e38b33bfab087a3a8e198068a2d8ee2bd73741e
ljw020711 <118701332+ljw020711@users.noreply.github.com>
2023-05-29 15:40:39 +0900

    Create 2493.cpp

---
+++ 스택/2493.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+int main() 
+{
+  ios::sync_with_stdio(0); cin.tie(0);
+  int num;
+  cin >> num;
+
+  stack<int> s;
+  stack<int> index;
+  s.push(100000001);
+  index.push(0);
+
+  int ans[num];
+  memset(ans, 0, sizeof(ans));
+  for(int i = 1; i <= num; i++)
+  {
+    int tower;
+    cin >> tower;
+    
+    while(s.top() < tower)
+    {
+      s.pop();
+      index.pop();
+    }
+    
+    s.push(tower);
+    cout << index.top() << ' ';
+    index.push(i);
+  }
+}
