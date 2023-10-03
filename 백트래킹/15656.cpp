3d48c4d716dde2b02ee2ae398fd7df515ac75744
ljw020711 <118701332+ljw020711@users.noreply.github.com>
2023-10-03 15:37:22 +0900

    Create 15656.cpp

---
+++ 백트래킹/15656.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+
+int num, len;
+int numbers[8];
+int ans[8];
+
+void func(int n, int available[])
+{
+  if(n == len)
+  {
+    for(int i = 0; i < len; i++)
+    {
+      cout << ans[i] << ' ';
+    }
+    cout << '\n';
+
+    return;
+  }
+
+  
+  for(int j = 0; j < num; j++)
+  {
+    available[j] = 1;
+    ans[n] = numbers[j];
+    func(n + 1, available);
+  }
+}
+
+int main() 
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> num >> len;
+  for(int i = 0; i < num; i++)
+  {
+    cin >> numbers[i];
+  }
+
+  sort(numbers, numbers + num);
+
+  int available[num];
+  memset(available, 0, sizeof(available));
+  
+  func(0, available);
+}
