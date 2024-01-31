0def8807f248032ba496f09b1acf28ac1310a043
ljw020711 <118701332+ljw020711@users.noreply.github.com>
2024-01-31 21:38:05 +0900

    Create 11650.cpp

---
+++ 정렬I/11650.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+vector<pair<int, int>> coordinate;
+vector<pair<int, int>> tmp;
+
+void merge(int st, int en)
+{
+  int mid = (st + en) / 2;
+
+  int a_idx = st;
+  int b_idx = mid;
+
+  for(int i = st; i < en; i++)
+  {
+    int x_a = coordinate[a_idx].first;
+    int y_a = coordinate[a_idx].second;
+    int x_b = coordinate[b_idx].first;
+    int y_b = coordinate[b_idx].second;
+    
+    if(a_idx == mid)
+    {
+      tmp.push_back({x_b, y_b});
+      b_idx++;
+    }
+    else if(b_idx == en)
+    {
+      tmp.push_back({x_a, y_a});
+      a_idx++;
+    }
+    else if(x_a < x_b || x_a == x_b && y_a <= y_b)
+    {
+      tmp.push_back({x_a, y_a});
+      a_idx++;
+    }
+    else
+    {
+      tmp.push_back({x_b, y_b});
+      b_idx++;
+    }
+  }
+
+  for(int i = st; i < en; i++)
+  {
+    coordinate[i] = tmp[i - st];
+  }
+
+  tmp.clear();
+}
+
+void mergesort(int st, int en)
+{
+  if(en - st == 1)
+  {
+    return;
+  }
+
+  int mid = (st + en) / 2;
+  mergesort(st, mid);
+  mergesort(mid, en);
+  merge(st, en);
+}
+
+int main() 
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n;
+  for(int i = 0; i < n; i++)
+  {
+    int x, y;
+    cin >> x >> y;
+
+    coordinate.push_back({x, y});
+  }
+
+  mergesort(0, n);
+
+  for(int i = 0; i < n; i++)
+  {
+    cout << coordinate[i].first << ' ' << coordinate[i].second << '\n';
+  }
+}
+
+/*
+사용한 벡터는 초기화 필요!!!!
+이걸로 벌써 몇번째 틀리는거지?
+
+꼭 좀 기억하자
+*/
